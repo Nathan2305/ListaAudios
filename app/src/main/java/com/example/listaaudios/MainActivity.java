c92692227c14424f7b608d9b72b81b94971cde1e
Jonathan <nathan20092411@gmail.com>
2019-06-05 23:04:09 -0500

    Se añade FAB

--- app/src/main/java/com/example/listaaudios/MainActivity.java
+++ app/src/main/java/com/example/listaaudios/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.listaaudios;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSeekBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.Toast;
 
 import java.io.File;
@@ -16,11 +19,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Button grabar, detener;
+    FloatingActionButton playStop;
     RecyclerView recycler;
     RecyclerView.LayoutManager layoutManager;
     static RecyclerView.Adapter adapter;
     public static String FOLDER_AUDIO = "";
+    public static AppCompatSeekBar seekBar;
     static File file;
     static ArrayList<String> listAudios = null;
 
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar=findViewById(R.id.seekBar);
         FOLDER_AUDIO = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Cibertec/Audios";
         file = new File(FOLDER_AUDIO);
         if (!file.exists()) {
@@ -50,9 +55,8 @@ public class MainActivity extends AppCompatActivity {
         recycler.setLayoutManager(layoutManager);
         recycler.setAdapter(adapter);
         recycler.hasFixedSize();
-        grabar = findViewById(R.id.grabar);
-        detener = findViewById(R.id.detener);
-        grabar.setOnClickListener(new View.OnClickListener() {
+        playStop=findViewById(R.id.playStop);
+        playStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intentRecordAudio = new Intent(getApplicationContext(), RecorderService.class);
@@ -60,12 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 startService(intentRecordAudio);
             }
         });
-        detener.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopService(new Intent(getApplicationContext(), RecorderService.class));
-            }
-        });
+
 
 
     }
--- app/src/main/java/com/example/listaaudios/RecorderService.java
+++ app/src/main/java/com/example/listaaudios/RecorderService.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
+import android.widget.MediaController;
+import android.widget.SeekBar;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,12 +17,15 @@ import java.text.DateFormat;
 import java.util.Date;
 
 import static com.example.listaaudios.MainActivity.FOLDER_AUDIO;
+import static com.example.listaaudios.MainActivity.seekBar;
 
 public class RecorderService extends Service implements MediaPlayer.OnPreparedListener {
     MediaRecorder mediaRecorder;
     MediaPlayer mediaPlayer;
     String nameAudio = "";
     File fileAudio = null;
+    Runnable runnable;
+    Handler handler;
     private static String OUTPUTFILE = "";
 
     public RecorderService() {
@@ -34,20 +40,7 @@ public class RecorderService extends Service implements MediaPlayer.OnPreparedLi
     @Override
     public void onCreate() {
         super.onCreate();
-        /*DateFormat sdf = DateFormat.getDateInstance();
-        DateFormat sdf2 = DateFormat.getTimeInstance();
-        Date date = new Date();
-        nameAudio = sdf.format(date) + "_" + sdf2.format(date) + ".mp3";
-        fileAudio = new File(FOLDER_AUDIO, nameAudio);
-        try {
-            if (fileAudio.createNewFile()) {
-                Utils.showToast(this, "Se creó archivo de audio");
-            } else {
-                Utils.showToast(this, "No se creó el archivo :" + nameAudio);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        handler=new Handler();
     }
 
     @Override
@@ -84,10 +77,29 @@ public class RecorderService extends Service implements MediaPlayer.OnPreparedLi
                 mediaPlayer.setOnPreparedListener(this);
                 mediaPlayer.setWakeMode(this, PowerManager.PARTIAL_WAKE_LOCK);
                 mediaPlayer.prepareAsync();
+
             } catch (IOException e) {
                 Utils.showToast(getApplicationContext(), "Error reproduciendo audio " + e.getMessage());
             }
         }
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (fromUser){
+                        mediaPlayer.seekTo(progress);
+                    }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         return START_STICKY;
     }
 
@@ -110,5 +122,21 @@ public class RecorderService extends Service implements MediaPlayer.OnPreparedLi
     @Override
     public void onPrepared(MediaPlayer mp) {
         mp.start();
+        seekBar.setMax(mp.getDuration());
+        changeSeekbar(mp);
     }
+
+    private void changeSeekbar(final MediaPlayer mp) {
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+        if (mp.isPlaying()) {
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    changeSeekbar(mp);
+                }
+            };
+            handler.postDelayed(runnable,1000);
+        }
+    }
+
 }
